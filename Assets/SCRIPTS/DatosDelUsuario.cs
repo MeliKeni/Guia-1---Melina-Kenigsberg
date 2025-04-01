f42c4d593f6655b523dabcbcefbe650a7b853b3b
MeliKeni <melinakenigsberg@gmail.com>
2025-04-01 09:04:13 -0300

    Empeze ej 1.2

---
+++ Assets/SCRIPTS/DatosDelUsuario.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DatosDelUsuario : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public string nombreUsuario;
+    public int edadUsuario;
+    public float estaturaUsuario;
+    public bool esDonante;
+
+    void Start()
+    {
+        Debug.Log("Mi nombre es " + nombreUsuario);        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
