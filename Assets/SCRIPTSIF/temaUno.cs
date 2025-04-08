05a0d833e137d087a48c5ad381be16d913c3a792
MeliKeni <melinakenigsberg@gmail.com>
2025-04-08 09:04:55 -0300

    Tema 1 ifs hecho

---
+++ Assets/SCRIPTSIF/temaUno.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class temaUno : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public string monedaDeseada;
+    public float montoAConvertir;
+    float montoConvertido;
+    float cotizacion;
+
+    void Start()
+    {
+        if(montoAConvertir < 1000)
+        {
+            Debug.Log("El monto minimo es 1000");
+            return;
+        }
+
+        
+        if (monedaDeseada == "D")
+        {
+            cotizacion = 1134.5f;
+        }
+        else if (monedaDeseada == "E")
+        {
+            cotizacion = 1397.16f;
+        } else if (monedaDeseada == "R")
+        {
+            cotizacion = 211.3f;
+        } else
+        {
+            Debug.Log("Opcion de moneda extranjera no valida");
+            return;
+        }
+
+
+        montoConvertido = montoAConvertir / cotizacion;
+        Debug.Log("Monto convertido: " + montoConvertido);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
